cea92a97f01752d236030498792077875569c8af
Adel1250 <adelashrafhamouda@gmail.com>
2023-09-21 21:26:37 +0300

    Refactor

--- binary-search/src/BinarySearch.java
+++ binary-search/src/BinarySearch.java
@@ -4,14 +4,6 @@ public class BinarySearch {
     public BinarySearch() {
     }
 
-    public BinarySearch(int[] arr) {
-        this.arr = arr;
-    }
-
-    public int[] getArr() {
-        return arr;
-    }
-
     public void setArr(int[] arr) {
         this.arr = arr;
     }
