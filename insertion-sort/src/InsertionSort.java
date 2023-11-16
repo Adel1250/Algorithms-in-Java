f79de96bf411957d71cad25684a9da85d6c40cf4
Adel1250 <adelashrafhamouda@gmail.com>
2023-11-16 21:50:07 +0200

    Uplaod insertion sort

---
+++ insertion-sort/src/InsertionSort.java
@@ -0,0 +1,15 @@
+public record InsertionSort(int[] arr) {
+    public void sort() {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ insertion-sort/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 1, 3};
+        InsertionSort insertionSort = new InsertionSort(arr);
+        insertionSort.sort();
+        System.out.println(Arrays.toString(arr));
+    }
+}
